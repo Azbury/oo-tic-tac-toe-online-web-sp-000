24fd14c0937e31444c31f1eadb980d9a372b4652
Austin Asbury <acasbury25@aol.com>
2019-08-29 14:44:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,4 +73,14 @@ class TicTacToe
       turn
     end
   end
+
+  def won?(board)
+    WIN_COMBINATIONS.each do |combo|
+      if (@board[combo[0]] === "X" && @board[combo[1]] === "X" && @board[combo[2]] === "X") || (@board[combo[0]] === "O" && @board[combo[1]] === "O" && @board[combo[2]] === "O")
+        return combo
+      end
+    end
+    false
+  end
+
 end
